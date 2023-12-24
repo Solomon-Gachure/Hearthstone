d4eef64b3383c340f9be2954cc9a9b34114870ac
Solomon Gachure <gachuresolomon64@gmail.com>
2023-12-24 15:03:02 +0300

    Slight changes

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      {/**hero section with the different wordings */}
+      <div>
+        <img  
+        src="https://img.freepik.com/free-photo/living-room-with-blue-couch-white-wall-with-painting-it_1340-23237.jpg?uid=R114683630&ga=GA1.1.134876288.1693412887&semt=ais_ai_generated"
+         alt="hero section" />
+         <div>
+          <h1>HEARTHSTONE APARTMENTS</h1>
+          <p>Welcome to our prestigious apartments</p>
+          <p>Where safe is home & home is safe!</p>
+         </div>
+      </div>
+    </div>
   )
 }
 
