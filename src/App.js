7b0a3866ff36d8f85ebf5f040fcfe37bd1802922
Solomon Gachure <gachuresolomon64@gmail.com>
2024-01-01 13:28:23 +0300

    Made slight changes to the app js to reflect the different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import About from "./components/about";
 import Home from "./components/home";
 import Navbar from "./components/navbar";
+import Services from "./components/services";
 
 function App() {
   return (
